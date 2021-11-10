ee39eeb632b68f2420f3455be34e3e3619638af3
Hitech2020 <73120571+Hitech2020@users.noreply.github.com>
2021-11-10 14:19:34 +0530

    saved

---
+++ listuser.py
@@ -0,0 +1,13 @@
+import boto3
+import sys
+
+accesskey = sys.argv[1]
+secretkey = sys.argv[2]
+
+client = boto3.client('iam', aws_access_key_id=accesskey,aws_secret_access_key=secretkey)
+
+var_List_users = client.list_users()
+#print (var_List_users['Users'])
+
+for new_var in var_List_users['Users']:
+	print (new_var['UserName'], new_var['CreateDate'])
